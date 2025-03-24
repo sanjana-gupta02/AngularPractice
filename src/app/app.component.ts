5b7dfac26d68be28fe801bab7c16355e314eb121
Sanjana Gupta <sanjanagupta1502@gmail.com>
2025-03-24 12:17:27 +0530

    Used Event Binding Technique to bind Click UC 3 Event to Launch BridgeLabz URL in a new Tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World';
-  imgUrl="../assets/BL_logo_square_jpg.jpg"
+  imgUrl="../assets/BL_logo_square_jpg.jpg";
+  url="https://www.bridgelabz.com";
 
   ngOnInit(): void{
-    this.title="Hello from BridgeLab."
-
+    this.title="Hello from BridgeLab.";
   }
-}
+
+
+  onClick($event:Event){
+      console.log("Save Button is clicked!", $event);
+      window.open(this.url,"_blank");
+    }
+  }
+
